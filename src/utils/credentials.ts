e226d2aae7b6a2e44d6781c53a4e817a7f4e304f
phlgr <philipp@gartz.dev>
2021-08-10 18:25:10 +0200

    ✨ Implement getCredential to get single credential

--- src/utils/credentials.ts
+++ src/utils/credentials.ts
@@ -7,3 +7,16 @@ export async function readCredentials(): Promise<Credential[]> {
   const credentials = db.credentials;
   return credentials;
 }
+
+export async function getCredential(service: string): Promise<Credential> {
+  const credentials = await readCredentials();
+  const credential = credentials.find(
+    (credential) => credential.service === service
+  );
+
+  if (!credential) {
+    throw new Error(`No credential found for service: ${service}`);
+  }
+
+  return credential;
+}
