84390c58e426be0000860474242751ae7e19c30a
phlgr <philipp@gartz.dev>
2021-08-10 18:23:30 +0200

    ✨ Add functionality for get route /api/credentials

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
 import express from 'express';
+import { readCredentials } from './utils/credentials';
 
 const app = express();
 const port = 3000;
+
+app.get('/api/credentials', async (_request, response) => {
+  const credentials = await readCredentials();
+
+  response.status(200).json(credentials);
+});
 
 app.get('/', (_request, response) => {
   response.send('Hello Credentials!');
