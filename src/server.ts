e0f334066b1b1c4204ef5ed1746bd6ebfd7060d8
marwinburesch <marwin@posteo.de>
2021-08-12 12:20:22 +0200

    :sparkles: Add PUT route to replace exiting credential

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import {
   deleteCredential,
   getCredential,
   readCredentials,
+  updateCredential,
 } from './utils/credentials';
 
 import type { Credential } from './types';
@@ -33,6 +34,18 @@ app.get('/api/credentials/:service', async (request, response) => {
   const { service } = request.params;
   try {
     const credential = await getCredential(service);
+    response.status(200).json(credential);
+  } catch (error) {
+    console.error(error);
+    response.status(404).send(`Could not find service: ${service}`);
+  }
+});
+
+app.put('/api/credentials/:service', async (request, response) => {
+  const { service } = request.params;
+  const credential: Credential = request.body;
+  try {
+    await updateCredential(service, credential);
     response.status(200).json(credential);
   } catch (error) {
     console.error(error);
