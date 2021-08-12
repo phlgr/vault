b9bff4747f5f37f5a40ca6297260a5c0e8278d71
marwinburesch <marwin@posteo.de>
2021-08-12 12:19:21 +0200

    :sparkles: Create updateCredential function

--- src/utils/credentials.ts
+++ src/utils/credentials.ts
@@ -40,3 +40,17 @@ export async function deleteCredential(service: string): Promise<void> {
   };
   await writeFile('src/db.json', JSON.stringify(newDB, null, 2));
 }
+
+export async function updateCredential(
+  service: string,
+  credential: Credential
+): Promise<void> {
+  const credentials = await readCredentials();
+  const filteredCredentials = credentials.filter(
+    (credential) => credential.service !== service
+  );
+  const newDB: DB = {
+    credentials: [...filteredCredentials, credential],
+  };
+  await writeFile('src/db.json', JSON.stringify(newDB, null, 2));
+}
