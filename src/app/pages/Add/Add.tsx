21651e60104227c05e84a1deba3cb16c7c720ca4
phlgr <philipp@gartz.dev>
2021-08-21 15:34:11 +0200

    ✨ Control input components on Add page

--- src/app/pages/Add/Add.tsx
+++ src/app/pages/Add/Add.tsx
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Add.module.css';
 
 export default function Add(): JSX.Element {
+  const [service, setService] = useState<string>('');
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [masterPassword, setMasterPassword] = useState<string>('');
+
   return (
     <main className={styles.container}>
       <h1>Vault</h1>
 
       <form className={styles.container}>
-        <input placeholder="Service" type="text" />
-        <input placeholder="User Name" type="text" />
-        <input placeholder="Password" type="password" />
-        <input placeholder="Master Password" type="password" />
+        <input
+          placeholder="Service"
+          type="text"
+          value={service}
+          onChange={(event) => setService(event.target.value)}
+          required
+        />
+        <input
+          placeholder="User Name"
+          type="text"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+          required
+        />
+        <input
+          placeholder="Password"
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          required
+        />
+        <input
+          placeholder="Master Password"
+          type="password"
+          value={masterPassword}
+          onChange={(event) => setMasterPassword(event.target.value)}
+          required
+        />
         <input type="submit" value="Save Credential" />
       </form>
     </main>
