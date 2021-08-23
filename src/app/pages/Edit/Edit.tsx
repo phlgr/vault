b3dea16de68afc1cf7dee2b1a9fc90716021f684
phlgr <philipp@gartz.dev>
2021-08-23 12:25:10 +0200

    ✨ Implement function to edit credential and handle error

--- src/app/pages/Edit/Edit.tsx
+++ src/app/pages/Edit/Edit.tsx
@@ -1,19 +1,54 @@
 import React, { useState } from 'react';
-import { useParams } from 'react-router';
+import { useHistory, useParams } from 'react-router';
+import { Credential } from '../../../types';
 import styles from './Edit.module.css';
 
 export default function Edit(): JSX.Element {
+  const history = useHistory();
   const { service: serviceParam }: { service: string } = useParams();
   const [service, setService] = useState<string>(serviceParam);
   const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const [masterPassword, setMasterPassword] = useState<string>('');
 
+  async function updateCredential(
+    credential: Credential,
+    masterPassword: string
+  ) {
+    const response = await fetch(`/api/credentials/${credential.service}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: masterPassword,
+      },
+      body: JSON.stringify(credential),
+    });
+    return response;
+  }
+
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const credential = {
+      service,
+      username,
+      password,
+    };
+    const response = await updateCredential(credential, masterPassword);
+    if (!response.ok) {
+      console.log('Something went wrong');
+      return;
+    }
+    history.push('/');
+  }
+
   return (
     <main className={styles.container}>
       <h1>Vault</h1>
       <p>Edit credential {serviceParam}</p>
-      <form className={styles.container}>
+      <form
+        className={styles.container}
+        onSubmit={(event) => handleSubmit(event)}
+      >
         <input
           type="text"
           placeholder="Service"
