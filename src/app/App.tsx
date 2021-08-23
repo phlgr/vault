052640d4da7546b36e18d5467b297af8568ea5d6
phlgr <philipp@gartz.dev>
2021-08-23 10:33:55 +0200

    ✨ Create new search route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Password from './pages/Password/Password';
+import Search from './pages/Search/Search';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/search">
+          <Search />
+        </Route>
         <Route path="/password/:service">
           <Password />
         </Route>
