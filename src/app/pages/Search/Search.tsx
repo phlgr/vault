1ba80fd588b41f89a4c56052209de183b0bbb948
phlgr <philipp@gartz.dev>
2021-08-23 10:34:36 +0200

    ✨ Control the inputs

--- src/app/pages/Search/Search.tsx
+++ src/app/pages/Search/Search.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Search.module.css';
 
 export default function Search(): JSX.Element {
+  const [service, setService] = useState<string>('');
+  const [masterPassword, setMasterPassword] = useState<string>('');
+
   return (
     <main className={styles.container}>
       <h1>Vault</h1>
       <form className={styles.container}>
-        <input type="text" placeholder="Service" />
-        <input type="text" placeholder="Master Password" />
+        <input
+          type="text"
+          placeholder="Service"
+          value={service}
+          onChange={(event) => setService(event.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Master Password"
+          value={masterPassword}
+          onChange={(event) => setMasterPassword(event.target.value)}
+        />
         <input type="submit" value="Submit" />
       </form>
     </main>
